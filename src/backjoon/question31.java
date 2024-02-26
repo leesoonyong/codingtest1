f5260f6bc938eabda6480ca3589ff440d7a0ee92
sylee <wnghkdcjdtns@naver.com>
2024-02-26 19:28:41 +0900

    백준 코밋

--- src/backjoon/question31.java
+++ src/backjoon/question31.java
@@ -4,6 +4,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -11,45 +12,40 @@ import java.util.StringTokenizer;
 public class question31 {
     /**
      *백준 11286
-     * 스택문제
+     * 절대값 힙
+     * 정렬문제
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1,o2) ->{
 
-        Stack<Integer> stack = new Stack<>();
 
-        while(N-- > 0){
-            st = new StringTokenizer(br.readLine());
-            int count = Integer.parseInt(st.nextToken());
-            if(count == 1){
-                stack.add(Integer.parseInt(st.nextToken()));
-            }else if(count == 4){
-                if(stack.isEmpty()){
-                    sb.append("1").append("\n");
+            //절대값 작은 데이터 먼우선
+            int frist_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+
+            //절대값이 같은 경우 음수 우선
+            if(frist_abs == second_abs){
+                return o1 > o2 ? 1 : -1;
+            }
+
+            return frist_abs - second_abs;
+        });
+
+        for(int i = 0; i < N; i++){
+            int request = Integer.parseInt(br.readLine());
+            if(request == 0){
+                if(myQueue.isEmpty()){
+                    System.out.println("0");
                 }else{
-                    sb.append("0").append("\n");
+                    System.out.println(myQueue.poll());
                 }
-            }else if(count == 2){
-                if(stack.isEmpty()){
-                    sb.append("-1").append("\n");
-                }else{
-                    sb.append(stack.pop()).append("\n");
-                }
-            }else if(count == 3){
-                sb.append(stack.size()).append("\n");
-            }else if(count == 5){
-                if(stack.isEmpty()) {
-                    sb.append("-1").append("\n");
-                }else{
-                    sb.append(stack.peek()).append("\n");
-                }
+            }else{
+                myQueue.add(request);
             }
         }
 
-        System.out.println(sb.toString());
 
     }
 }
